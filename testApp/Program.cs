a9f468b40719286d0dc1ea8402b270bf2e38332b
shigobu <shigobu@shigobu.com>
2021-09-24 00:02:42 +0900

    プロジェクト ファイルを追加します。

---
+++ testApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnglishKana;
+
+namespace testApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EnglishKanaDictionary e2k = new EnglishKanaDictionary();
+            while (true)
+            {
+                string readString = Console.ReadLine();
+                Console.WriteLine(e2k.ReplaceEnglishToKana(readString));
+                if (readString == "exit")
+                {
+                    System.Threading.Thread.Sleep(1000);
+                    break;
+                }
+            }
+        }
+    }
+}
